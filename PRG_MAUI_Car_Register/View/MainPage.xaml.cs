ece42e4141fb4e79fad41e5abdbd7c1e790fd5f5
iwantmoneyplease <122149540+iwantmoneyplease@users.noreply.github.com>
2025-10-22 11:18:00 +0200

    Fixed issue with platform usage
    Issues with "using" fixed

--- PRG_MAUI_Car_Register/View/MainPage.xaml.cs
+++ PRG_MAUI_Car_Register/View/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PRG_MAUI_Car_Register.Model;
+using PRG_MAUI_Car_Register.ViewModel;
 
 namespace PRG_MAUI_Car_Register
 {
@@ -18,36 +19,58 @@ namespace PRG_MAUI_Car_Register
             {
                 try
                 {
-                    Vehicle vehicle;
+                    Car car = new Car();
+                    Motorcycle motorcycle = new Motorcycle();
+                    Truck truck = new Truck();
 
                     switch ((Vehicle.Type)pickerType.SelectedIndex)
                     {
                         case Vehicle.Type.Bil:
                         {
-                            vehicle = new Car();
-                            break;
+                            car = new Car();
+
+                                string regNr = entryRegistrationNumber.Text;
+                                car.RegistrationNumber = regNr;
+                                car.Manufacturer = entryManufacturer.Text;
+                                car.Model = entryModel.Text;
+                                car.ModelYear = entryModelYear.Text;
+
+                                vehicleList.Add(car);
+                                listViewVehicles.ItemsSource = null;
+                                listViewVehicles.ItemsSource = vehicleList;
+                                break;
                         }
                         case Vehicle.Type.MC:
                         {
-                            vehicle = new Motorcycle();
-                            break;
+                            motorcycle = new Motorcycle();
+
+                                string regNr = entryRegistrationNumber.Text;
+                                motorcycle.RegistrationNumber = regNr;
+                                motorcycle.Manufacturer = entryManufacturer.Text;
+                                motorcycle.Model = entryModel.Text;
+                                motorcycle.ModelYear = entryModelYear.Text;
+
+                                vehicleList.Add(motorcycle);
+                                listViewVehicles.ItemsSource = null;
+                                listViewVehicles.ItemsSource = vehicleList;
+                                break;
                         }
                         case Vehicle.Type.Lastbil:
                         {
-                            vehicle = new Truck();
-                            break;
+                            truck = new Truck();
+
+                                string regNr = entryRegistrationNumber.Text;
+                                truck.RegistrationNumber = regNr;
+                                truck.Manufacturer = entryManufacturer.Text;
+                                truck.Model = entryModel.Text;
+                                truck.ModelYear = entryModelYear.Text;
+
+                                vehicleList.Add(truck);
+                                listViewVehicles.ItemsSource = null;
+                                listViewVehicles.ItemsSource = vehicleList;
+                                break;
                         }
                     }
-
-                    string regNr = entryRegistrationNumber.Text;
-                    vehicle.RegistrationNumber = regNr;
-                    vehicle.Manufacturer = entryManufacturer.Text;
-                    vehicle.Model = entryModel.Text;
-                    vehicle.ModelYear = entryModelYear.Text;
-
-                    vehicleList.Add(vehicle);
-                    listViewVehicles.ItemsSource = null;
-                    listViewVehicles.ItemsSource = vehicleList;
 
                     entryRegistrationNumber.Text = string.Empty;
                     entryManufacturer.Text = string.Empty;
---
+++ PRG_MAUI_Car_Register/ViewModel/VehicleService.cs
@@ -0,0 +1,34 @@
+﻿using PRG_MAUI_Car_Register.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace PRG_MAUI_Car_Register.ViewModel
+{
+    class VehicleService
+    {
+        // för att säkerställa singleton (se ovan)
+        private static VehicleService _instance;
+        public static VehicleService Instance => _instance ??= new VehicleService();
+
+        // själva listan
+        public ObservableCollection<Vehicle> VehicleItems { get; set; }
+
+        // några defaultvärden (ta bort denna sen)
+        private VehicleService()
+        {
+            VehicleItems = new ObservableCollection<Vehicle>
+            {
+            new Car(),
+            new Motorcycle(),
+            new Truck(),
+            };
+        }
+    }
+
+}
