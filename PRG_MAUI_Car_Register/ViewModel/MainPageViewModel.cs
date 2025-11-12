266002f9c85b62e4d7e22c70dedb5ebe900a0f39
iwantmoneyplease <122149540+iwantmoneyplease@users.noreply.github.com>
2025-11-12 11:14:22 +0100

    Added MainPageViewModel.cs

---
+++ PRG_MAUI_Car_Register/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_MAUI_Car_Register.ViewModel
+{
+    internal class MainPageViewModel
+    {
+    }
+}
