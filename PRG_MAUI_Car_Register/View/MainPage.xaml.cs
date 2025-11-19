92b503196ab60c88e272ad5f9b1ff4930a175bbc
iwantmoneyplease <122149540+iwantmoneyplease@users.noreply.github.com>
2025-11-19 11:25:21 +0100

    Added binding, need to fix onproperty changed

--- PRG_MAUI_Car_Register/View/MainPage.xaml.cs
+++ PRG_MAUI_Car_Register/View/MainPage.xaml.cs
@@ -1,133 +1,15 @@
 ﻿using PRG_MAUI_Car_Register.Model;
 using PRG_MAUI_Car_Register.ViewModel;
+using PRG_MAUI_Car_Register.ViewModel.PRG_MAUI_Car_Register.ViewModel;
 
 namespace PRG_MAUI_Car_Register
 {
     public partial class MainPage : ContentPage
     {
-        List<Vehicle> vehicleList = new List<Vehicle>();
-
         public MainPage()
         {
             InitializeComponent();
-            pickerType.SelectedIndex = 0;
-        }
-
-        private void OnRegisterClicked(object sender, EventArgs e)
-        {
-            if (entryManufacturer.Text != "" && entryModel.Text != "")
-            {
-                try
-                {
-                    Vehicle vehicle;
-
-                    switch ((Vehicle.Type)pickerType.SelectedIndex)
-                    {
-                        case Vehicle.Type.Bil:
-
-                            vehicle = new Car();
-                            vehicle.RegistrationNumber = entryRegistrationNumber.Text;
-                            vehicle.Manufacturer = entryManufacturer.Text;
-                            vehicle.Model = entryModel.Text;
-                            vehicle.ModelYear = entryModelYear.Text;
-                            break;
-
-                        case Vehicle.Type.MC:
-
-                            vehicle = new Motorcycle();
-                            vehicle.RegistrationNumber = entryRegistrationNumber.Text;
-                            vehicle.Manufacturer = entryManufacturer.Text;
-                            vehicle.Model = entryModel.Text;
-                            vehicle.ModelYear = entryModelYear.Text;
-
-                            break;
-
-
-                        case Vehicle.Type.Lastbil:
-
-                            vehicle = new Truck();
-                            vehicle.RegistrationNumber = entryRegistrationNumber.Text;
-                            vehicle.Manufacturer = entryManufacturer.Text;
-                            vehicle.Model = entryModel.Text;
-                            vehicle.ModelYear = entryModelYear.Text;
-                            break;
-
-                        default:
-                            throw new Exception("Okänd fordonstyp vald.");
-                    }
-                    vehicleList.Add(vehicle);
-                    listViewVehicles.ItemsSource = null;
-                    listViewVehicles.ItemsSource = vehicleList;
-
-                    ClearEntryFields();
-                }
-                catch (ArgumentException ex)
-                {
-                    DisplayAlert("Fel", ex.Message, "OK");
-                }
-            }
-        }
-
-        private void OnRadioCheckedChanged(object sender, CheckedChangedEventArgs e)
-        {
-            if (e.Value != true) return;
-
-            // Skapa en filtrerad lista baserat på vilken radioknapp som är vald
-            List<Vehicle> filteredList;
-
-            if (radioCar.IsChecked)
-            {
-                filteredList = vehicleList.Where(v => v.VehicleType == Vehicle.Type.Bil).ToList();
-            }
-            else if (radioMC.IsChecked)
-            {
-                filteredList = vehicleList.Where(v => v.VehicleType == Vehicle.Type.MC).ToList();
-            }
-            else if (radioTruck.IsChecked)
-            {
-                filteredList = vehicleList.Where(v => v.VehicleType == Vehicle.Type.Lastbil).ToList();
-            }
-            else
-            {
-                // Om "Alla" är vald, visa hela listan
-                filteredList = vehicleList;
-            }
-
-            listViewVehicles.ItemsSource = filteredList;
-        }
-
-        private void OnSearchClicked(object sender, EventArgs e)
-        {
-            string searchTerm = entrySearchRegistrationNumber.Text?.ToLower();
-
-            if (string.IsNullOrEmpty(searchTerm))
-            {
-                entrySearchRegistrationNumber.Text = "Ange ett registreringsnummer för att söka.";
-                return;
-            }
-
-            var foundVehicle = vehicleList.FirstOrDefault(v => v.RegistrationNumber?.ToLower() == searchTerm);
-
-            if (foundVehicle != null)
-            {
-                labelSearchResult.Text = $"Fordon hittat:\n" +
-                                         $"Registreringsnummer: {foundVehicle.RegistrationNumber}\n" +
-                                         $"Tillverkare: {foundVehicle.Manufacturer}\n" +
-                                         $"Modell: {foundVehicle.Model}\n" +
-                                         $"Typ: {foundVehicle.VehicleType}\n" +
-                                         $"År: {foundVehicle.ModelYear}";
-            }
-            else
-            {
-                labelSearchResult.Text = "Inget fordon hittades med det registreringsnumret.";
-            }
-        }
-        private void ClearEntryFields()
-        {
-            entryRegistrationNumber.Text = string.Empty;
-            entryManufacturer.Text = string.Empty;
-            entryModel.Text = string.Empty;
-            entryModelYear.Text = string.Empty;
+            BindingContext = new MainPageViewModel();
         }
     }
 }
--- PRG_MAUI_Car_Register/ViewModel/MainPageViewModel.cs
+++ PRG_MAUI_Car_Register/ViewModel/MainPageViewModel.cs
@@ -1,12 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PRG_MAUI_Car_Register.ViewModel
 {
-    internal class MainPageViewModel
+    namespace PRG_MAUI_Car_Register.ViewModel
     {
+        class MainPageViewModel : INotifyPropertyChanged
+        {
+            //on property changed
+            //public event PropertyChangedEventHandler PropertyChanged;
+            //void OnPropertyChanged([CallerMemberName] string name = null)
+            //    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+            private string _registrationNumber;
+            public string RegistrationNumber
+            {
+                get => _registrationNumber;
+                set { _registrationNumber = value; OnPropertyChanged(); }
+            }
+
+            private string _manufacturer;
+            public string Manufacturer
+            {
+                get => _manufacturer;
+                set { _manufacturer = value; OnPropertyChanged(); }
+            }
+
+            private string _model;
+            public string Model
+            {
+                get => _model;
+                set { _model = value; OnPropertyChanged(); }
+            }
+
+            private string _modelYear;
+            public string ModelYear
+            {
+                get => _modelYear;
+                set { _modelYear = value; OnPropertyChanged(); }
+            }
+
+            private string _selectedType;
+            public string SelectedType
+            {
+                get => _selectedType;
+                set { _selectedType = value; OnPropertyChanged(); }
+            }
+
+            public ObservableCollection<Vehicle> Vehicles => VehicleService.Instance.VehicleItems;
+
+            public ICommand RegisterCommand { get; }
+            public ICommand SearchCommand { get; }
+
+            private string _searchQuery;
+            public string SearchQuery
+            {
+                get => _searchQuery;
+                set { _searchQuery = value; 
+                    OnPropertyChanged(); }
+            }
+
+            private string _searchResult;
+            public string SearchResult
+            {
+                get => _searchResult;
+                set { _searchResult = value; 
+                    OnPropertyChanged(); }
+            }
+
+
+            public MainPageViewModel()
+            {
+                RegisterCommand = new Command(RegisterVehicle);
+                SearchCommand = new Command(SearchVehicle);
+            }
+
+            private void RegisterVehicle()
+            {
+                Vehicle v = SelectedType switch
+                {
+                    "Bil" => new Car(),
+                    "MC" => new Motorcycle(),
+                    "Lastbil" => new Truck(),
+                    _ => null
+                };
+
+                if (v == null) return;
+
+                try
+                {
+                    v.RegistrationNumber = RegistrationNumber;
+                    v.Manufacturer = Manufacturer;
+                    v.Model = Model;
+                    v.ModelYear = ModelYear;
+
+                    Vehicles.Add(v);
+
+                    RegistrationNumber = "";
+                    Manufacturer = "";
+                    Model = "";
+                    ModelYear = "";
+                    SelectedType = null;
+                }
+                catch (Exception ex)
+                {
+                    SearchResult = ex.Message;
+                }
+            }
+
+            private void SearchVehicle()
+            {
+                var result = Vehicles.FirstOrDefault(v => v.RegistrationNumber == SearchQuery);
+
+                SearchResult = result == null
+                    ? "Inget fordon hittades."
+                    : $"{result.Manufacturer} {result.Model} ({result.ModelYear})";
+            }
+        }
     }
 }
